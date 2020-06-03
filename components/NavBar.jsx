9442b6d541008af77f2172a960864e14f6c50ccf
bhillier3 <bhillier3@gatech.edu>
2020-06-02 23:17:40 -0700

    create NavBar and add styling

---
+++ components/NavBar.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import styles from '../styles/navbar.module.css'
+
+const NavBar = () => {
+
+  return (
+    <div className={styles.navbar}>
+      <p className={styles.info}>
+        828-595-2748 | 840 Suite 5 Fleming St., Hendersonville, NC 28791
+      </p>
+      <h3 className={styles.logo}>THE SMART PERSON</h3>
+      <ul className={styles.list}>
+        <li className={styles.li}><Link href="/">
+          <a className={styles.link}>Home</a>
+        </Link></li>
+        <li className={styles.li}><Link href="/about">
+          <a className={styles.link}>About</a>
+        </Link></li>
+        <li className={styles.li}><Link href="/treaments">
+          <a className={styles.link}>Treatments</a>
+        </Link></li>
+        <li className={styles.li}><Link href="/appointments">
+          <a className={styles.link}>Appointments</a>
+        </Link></li>
+        <li className={styles.li}><Link href="/contact">
+          <a className={styles.link}>Contact</a>
+        </Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar;
