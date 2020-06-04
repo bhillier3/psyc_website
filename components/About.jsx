1b54413e300ac61c700365e0313500842da1aa05
bhillier3 <bhillier3@gatech.edu>
2020-06-03 23:18:17 -0700

    begin about section

--- components/About.jsx
+++ components/About.jsx
@@ -1,10 +1,14 @@
-
+import styles from '../styles/about.module.css';
+import utils from '../styles/utils.module.css';
 
 const About = () => {
 
   return (
-    <section className="about">
-
+    <section className={styles.wrapper}>
+      <div className={styles.header}>
+        <h3 className={utils.section_title}>ABOUT</h3>
+        <h1 className={styles.name}>SHAWN WILLSON M.D.</h1>
+      </div>
     </section>
   )
 }
--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,14 +1,9 @@
-import styles from '../styles/homepage.module.css'
+import styles from '../styles/homescreen.module.css'
 
 const HomePage = () => {
 
   return (
     <section className={styles.home}>
-      {/* <img 
-        src="/images/mountain_lake_background.jpg"
-        className={styles.image}
-        alt="mountain lake background"
-      /> */}
       <div className={styles.text_container}>
         <h1 className={styles.titlea}>INNOVATIVE PSYCHIATRIST</h1>
         <h3 className={styles.titleb}>WITH</h3>
