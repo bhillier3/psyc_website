af326cda20083aa191dec7c5b45c1fc82aeea48d
bhillier3 <bhillier3@gatech.edu>
2020-06-11 10:50:24 -0700

    content changes to page and updated contact faq

--- components/Info.jsx
+++ components/Info.jsx
@@ -15,6 +15,11 @@ const Info = () => {
         <h3 className={utils.small_title}>Patient Information</h3>
         <ul className={styles.list}>
           <li className={styles.li}>
+            <h5>Do you accept insurance?</h5>
+            <p>Medicare is the only accepted form of insurace.</p>
+            <p>Dr. Willson performs a thorough, comprehensive assessment of each patient which is clearly documented on the patient's superbill (receipt) for their insurance provider. We can then assist in getting you a generous reimbursement from your insurance.</p>
+          </li>
+          <li className={styles.li}>
             <h5>What forms of payment are accepted?</h5>
             <p>Cash and credit card payments are accepted.</p>
             <p>Credit card information must be collected prior to your initial appointment.</p>
@@ -25,16 +30,12 @@ const Info = () => {
             <p>You will be charged a cancellation fee if you cancel within 24 hours of your appointment.</p>
           </li>
           <li className={styles.li}>
-            <h5>Do you accept insurance?</h5>
-            <p>Medicare is the only accepted form of insurace.</p>
-          </li>
-          <li className={styles.li}>
             <h5>Do I need to fill out any forms prior to my appointment?</h5>
             <p>Yes, you must fill out all of the required forms prior to your first appointment.</p>
             <p>
-              <span>The required forms can be found </span>
-              <span><Link href="/"><a>here</a></Link></span>
-              <span>.</span>
+              <span>
+                The required forms can be found <Link href="/"><a>here</a></Link>.
+              </span>
             </p>
           </li>
         </ul>
@@ -43,12 +44,18 @@ const Info = () => {
         <h3 className={utils.small_title}>Contact</h3>
         <ul className={styles.list}>
             <li className={styles.li}>
-              <h5>Stuff</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, tempora!</p>
+              <h5>How can I schedule an appointment?</h5>
+              <p>
+                <span>
+                  You can schedule your appointment with Simple Practice <Link href="/"><a>here</a></Link>.
+                </span>
+              </p>
+              <p>You can also call the office at <b>828-549-8082</b> to schedule appointments by phone.</p>
             </li>
             <li className={styles.li}>
-              <h5>Stuff</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, tempora!</p>
+              <h5>What if it's an emergency?</h5>
+              <p>If you require urgent care you may contact Dr. Willson directly at <em>913-948-1340</em>.</p>
+              <p>Continual and repeated contact to Dr. Willson can result in incurred fees.</p>
             </li>
             <li className={styles.li}>
               <h5>Stuff</h5>
