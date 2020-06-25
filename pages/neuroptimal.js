af8df371d58f7c10b7b0a3f51e51741541e28b54
bhillier3 <bhillier3@gatech.edu>
2020-06-25 11:17:12 -0700

    completed nlp and neuroptimal pages

--- pages/neuroptimal.js
+++ pages/neuroptimal.js
@@ -1,10 +1,15 @@
 import Head from 'next/head';
 import NavBar from '../components/NavBar';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import nlp_styles from '../styles/info_pages.module.css';
+import styles from '../styles/info.module.css';
+import utils from '../styles/utils.module.css';
 
-export default function Neuroptimal() {
+export default function NLP() {
 
   return (
-    <div className="container">
+    <div className={nlp_styles.wrapper}>
       <Head>
         <title>Smart Person LLC</title>
         <link
@@ -13,10 +18,19 @@ export default function Neuroptimal() {
         </link>
       </Head>
       <NavBar />
-      <img
-        src="/images/neuroptimal_page.jpg"
-        alt="photo"
-      />
+      <div className={`${styles.wrapper} ${nlp_styles.container}`}>
+        <img
+          src="/images/neuroptimal_page.jpg"
+          alt="photo"
+          className={styles.image}
+        />
+        <div className={styles.container}>
+          <p className={utils.small_title}>Neuroptimal</p>
+          <p className={nlp_styles.text}>Neuroptimal is an improved type of neurofeedback. Electrons placed on your head connect your brain to an amplified software. The electrodes measure all of your brain waves and map them onto a computer screen. The software interprets your brain waves and then provides feedback to your brain. The feedback enables your brain to release any patterns that are not working properly and to be open to new patterns for your desirable outcome.Neuroptimal can help coordination and sports performance. Be the best you.</p>
+        </div>
+      </div>
+      <Contact />
+      <Footer />
     </div>
   )
 }
--- pages/nlp.js
+++ pages/nlp.js
@@ -1,11 +1,15 @@
 import Head from 'next/head';
 import NavBar from '../components/NavBar';
-import styles from '../styles/nlp_page.module.css';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import nlp_styles from '../styles/info_pages.module.css';
+import styles from '../styles/info.module.css';
+import utils from '../styles/utils.module.css';
 
 export default function NLP() {
 
   return (
-    <div className={styles.container}>
+    <div className={nlp_styles.wrapper}>
       <Head>
         <title>Smart Person LLC</title>
         <link
@@ -14,22 +18,26 @@ export default function NLP() {
         </link>
       </Head>
       <NavBar />
-      <img
-        src="/images/NLP_page.jpg"
-        alt="photo"
-        className={styles.image}
-      />
-      <div className={styles.text}>
-        <p className={styles.title}>NLP is:</p>
-        <ul className={styles.list}>
-          <li className={styles.li}>Neuro: related to the brain</li>
-          <li className={styles.li}>
-            Linguistic: the language or self-talk we use
-          </li>
-          <li className={styles.li}>Programming: sequences and anchors our actions to achieve our desired outcome</li>
-        </ul>
-        <p>Dr. Willson guides you through this process in addition to other treatment(s). She will help you build a skill to manage and combat your every day periods of negativity. Soon the skill will become a regular habit and significantly improve your well-being.</p>
+      <div className={`${styles.wrapper} ${nlp_styles.container}`}>
+        <img
+          src="/images/NLP_page.jpg"
+          alt="photo"
+          className={styles.image}
+        />
+        <div className={styles.container}>
+          <p className={utils.small_title}>NLP</p>
+          <ul className={styles.list}>
+            <li className={`${styles.li} ${nlp_styles.li}`}><b>Neuro</b>: related to the brain</li>
+            <li className={`${styles.li} ${nlp_styles.li}`}>
+              <b>Linguistic</b>: the language or self-talk we use
+            </li>
+            <li className={`${styles.li} ${nlp_styles.li}`}><b>Programming</b>: sequences and anchors our actions to achieve our desired outcome</li>
+          </ul>
+          <p className={nlp_styles.text}>Dr. Willson guides you through this process in addition to other treatment(s). She will help you build a skill to manage and combat your every day periods of negativity. Soon the skill will become a regular habit and significantly improve your well-being.</p>
+        </div>
       </div>
+      <Contact />
+      <Footer />
     </div>
   )
 }
